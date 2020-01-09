9b9129773e601b7fd22ea593416e231534675675
Brike <Brike123456789panyawit@gmail.com>
2020-01-09 16:24:40 +0700

    add src

---
+++ src/profit/Salary.java
@@ -0,0 +1,28 @@
+package profit;
+
+public class Salary {
+	public double salary;
+	public double commission;
+	public Salary(double salary, double commission) {
+		super();
+		this.salary = salary;
+		this.commission = commission;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public double getCommission() {
+		return commission;
+	}
+	public void setCommission(double commission) {
+		this.commission = commission;
+	}
+	@Override
+	public String toString() {
+		return "Salary [salary=" + salary + ", commission=" + commission + "]";
+	}
+	
+}
